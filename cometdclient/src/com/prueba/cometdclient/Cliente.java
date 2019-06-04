648b8f4d0e2bd748ed298fc4a589a202cc2fa7c2
adrycarou896 <adripc@live.com>
2019-06-05 00:42:14 +0200

    Ya no hay errores de compilación

--- cometdclient/src/com/prueba/cometdclient/Cliente.java
+++ cometdclient/src/com/prueba/cometdclient/Cliente.java
@@ -8,13 +8,30 @@ import org.cometd.bayeux.client.ClientSessionChannel;
 import org.cometd.bayeux.client.ClientSessionChannel.MessageListener;
 import org.cometd.client.BayeuxClient;
 import org.cometd.client.transport.LongPollingTransport;
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.api.Request;
 
 public class Cliente {
-
+	
 	public static void main(String[] args) {
 		
-		String url = "http://localhost:8080/cometd/cometd";
-		 BayeuxClient client = new BayeuxClient(url, new LongPollingTransport());
+		String cometdURL = "http://localhost:8080/cometd/cometd";
+		
+		 HttpClient httpClient = new HttpClient();
+		 
+		 Map<String, Object> options = new HashMap<>();
+
+		  LongPollingTransport transport = new LongPollingTransport(options, httpClient) {
+
+		    @Override
+		    protected void customize(Request request) {
+		      super.customize(request);
+		    }
+		  };	       
+		  
+		  BayeuxClient client = new BayeuxClient(cometdURL, transport);
+		
+		 //BayeuxClient client = new BayeuxClient(url, new LongPollingTransport());
 		 client.handshake();
 		 client.waitFor(1000, BayeuxClient.State.CONNECTED);
 
