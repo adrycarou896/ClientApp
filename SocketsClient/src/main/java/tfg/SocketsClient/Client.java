66cb12a7aaf9175e6eef7e2b063b8352af9dd4d5
adrycarou896 <adripc@live.com>
2019-07-05 00:15:01 +0200

    Mejoras

--- SocketsClient/src/main/java/tfg/SocketsClient/Client.java
+++ SocketsClient/src/main/java/tfg/SocketsClient/Client.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Date;
+
+import org.json.JSONObject;
 
 /**
  * Hello world!
@@ -17,8 +20,24 @@ public class Client
     		Socket s = new Socket("localhost", 4999);
         	ObjectInputStream input = new ObjectInputStream(s.getInputStream());
         	try {
-    			String personJSON = String.valueOf(input.readObject());
-    			System.out.println(personJSON);
+    			String dataJSON = String.valueOf(input.readObject());
+    			//System.out.println(dataJSON);
+    			
+    			JSONObject data = new JSONObject(dataJSON);
+    			
+    			JSONObject personJSON = (JSONObject) data.get("person");
+    			String personName = personJSON.getString("name");
+    			
+    			JSONObject eventJSON = (JSONObject) data.get("event");
+    			Date dateEvent = new Date();
+    			dateEvent.setTime(eventJSON.getLong("date"));
+    			String messageEvent = eventJSON.getString("mensaje");
+    			
+    			System.out.println(personName+" a las "+dateEvent.toString()+" "+messageEvent);
+    			System.out.println(eventJSON.toString());
+    			
+    			
+    			
     		} catch (ClassNotFoundException e) {
     			e.printStackTrace();
     		}
